1e41aa3c063ea2de62c868c6ddf735378fb1af42
Joaquin Andino <andino.joaquin.ezequiel@gmail.com>
2025-10-13 20:20:04 -0300

    se pasa la informacion del item desde el index al producto page

--- scripts.js
+++ scripts.js
@@ -1,45 +1,47 @@
-const obtenerProductos = async () => {
+const obtenerItems = async () => {
   //obtiene los item del archivo y luego lo parsea de json (aca luego va la api del backend)
-  const productos = await fetch("productos.json").then((res) => res.json());
-  console.log(productos);
+  const items = await fetch("items.json").then((res) => res.json());
+  console.log(items);
 
-  //este va a ser la seccion que contenga los producto. si o si hay que seleccionar por id
-  const seccionProductos = document.getElementById("productos"); //este va a ser el elemento html que va a tener de hijo al html de los prodcutos
+  //este va a ser la seccion que contenga los items. si o si hay que seleccionar por id
+  const seccionItems = document.getElementById("items"); //este va a ser el elemento html que va a tener de hijo al html de los prodcutos
 
-  //luego, por cada producto, craftea un html
-  productos.forEach((producto) => {
-    //va a ser una 'a' porque queremos que todo lo que se muestre del producto lo redireccion a la su pagina correspondiente
+  //luego, por cada item, craftea un html
+  items.forEach((item) => {
+    //va a ser una 'a' porque queremos que todo lo que se muestre del item lo redireccion a la su pagina correspondiente
     const a = document.createElement("a");
     //le agregamos los estilos
     a.classList.add("item", "fade");
     a.href = "product.html";
-    a.target = "_blank";
-    a.setAttribute("data-plataforma", producto.plataforma);
+    a.setAttribute("data-plataforma", item.plataforma);
+    a.addEventListener("click", () => {
+      localStorage.setItem("item", JSON.stringify(item));
+    });
 
-    //imagen del producto
+    //imagen del item
     const img = document.createElement("img");
     //esto es para indicar que la imagen que contendra la etiqueta es la del .json
-    img.src = producto.image;
+    img.src = item.image;
     img.classList.add("item-imagen");
 
     //etiqueta que contendra el texto del nombre y el precio
     const contInfo = document.createElement("div");
     contInfo.classList.add("item-info");
 
-    const nombreProducto = document.createElement("p");
-    nombreProducto.classList.add("item-nombre");
+    const nombreItem = document.createElement("p");
+    nombreItem.classList.add("item-nombre");
     //esto es para indicar que el texto que contendra la etiqueta proviene del .json
-    nombreProducto.textContent = `${producto.nombre}`;
+    nombreItem.textContent = `${item.nombre}`;
 
     const nombrePrecio = document.createElement("p");
     nombrePrecio.classList.add("item-precio");
-    nombrePrecio.textContent = `$${producto.precio}`;
+    nombrePrecio.textContent = `$${item.precio}`;
 
     //ahora se arma el html
-    seccionProductos.appendChild(a);
+    seccionItems.appendChild(a);
     a.appendChild(img);
     a.appendChild(contInfo);
-    contInfo.appendChild(nombreProducto);
+    contInfo.appendChild(nombreItem);
     contInfo.appendChild(nombrePrecio);
   });
 };
@@ -66,3 +68,24 @@ const filtrarPorPlataforma = () => {
     });
   });
 };
+
+//function porque con const () => no se porque no anda
+function renderizarItemSeleccionado() {
+  //toma el item guardado en el local storage
+  const item = JSON.parse(localStorage.getItem("item"));
+  //luego agrega el contenido en el html
+  const imagen = document.querySelector(".item-image img");
+  imagen.src = item.image;
+  imagen.alt = item.nombre;
+
+  const titulo = document.querySelector(".item-title");
+  titulo.textContent = item.nombre;
+
+  const precio = document.querySelector(".item-precio-destacado");
+  precio.textContent = `$ ${item.precio}`;
+
+  const carac = document.querySelector(".item-caracteristicas");
+  carac.textContent = `${item.plataforma}`;
+  const desc = document.querySelector(".item-descripcion");
+  desc.textContent = `${item.nombre}`;
+}
